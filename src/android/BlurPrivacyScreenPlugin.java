56b562dc7609de3f2c05f462df752c7fb82d0117
LlamaloX <sergioazpiazu@gmail.com>
2021-02-12 14:52:10 -0500

    Update BlurPrivacyScreenPlugin.java

--- src/android/BlurPrivacyScreenPlugin.java
+++ src/android/BlurPrivacyScreenPlugin.java
@@ -31,15 +31,18 @@ public class BlurPrivacyScreenPlugin extends CordovaPlugin {
     super.initialize(cordova, webView);
   
   }
-  
+	
+
 @Override
 public void onPause(boolean multitasking) {
 		Activity activity = this.cordova.getActivity();
-		activity.getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+		activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+	super.onPause();
 	}
 	
 	@Override
 	public void onResume(boolean multitasking) {
+		super.onResume();
 		Activity activity = this.cordova.getActivity();
 		activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SECURE);		
 	}
